7296d1a76a38a49ac493e73d33fc4f21fcc3af65
katsu1110 <code1110g-show@hotmail.co.jp>
2018-02-17 10:01:17 +0100

    correlated bivariate dist

--- bayesbrain/mcmc.py
+++ bayesbrain/mcmc.py
@@ -15,7 +15,7 @@ def target_pdf(type):
     if type == 'round':
         x, y = np.random.multivariate_normal([2,2], [[1,0],[0,1]], 5000).T
     elif type == 'correlated':
-        x, y = np.random.multivariate_normal([2,2], [[1,0],[0,50]], 5000).T
+        x, y = np.random.multivariate_normal([2,2], [[1, 0.9], [0.9, 1]], 5000).T
     elif type == 'bimodal':
         x, y = 0.5*np.random.multivariate_normal([1,1], [[1,0],[0,1]], 5000).T \
             + 0.5*np.random.multivariate_normal([3,3], [[1,0],[0,1]], 5000).T
@@ -25,7 +25,17 @@ def target_pdf(type):
     return x, y
 
 # visualization similar to Fig 1b in the Sanborn & Charter 2016
-fig, ax = plt.subplots(4, 3, sharex=True, sharey=True)
+type = ['round', 'correlated', 'bimodal', 'close_bimodal']
+plt.close('all')
+fig, axes = plt.subplots(1, 4)
+x, y = target_pdf(type[0])
+axes[0].plot(x,y,'o')
+x, y = target_pdf(type[1])
+axes[1].plot(x,y,'o')
+x, y = target_pdf(type[2])
+axes[2].plot(x,y,'o')
+x, y = target_pdf(type[3])
+axes[3].plot(x,y,'o')
 
 
 
