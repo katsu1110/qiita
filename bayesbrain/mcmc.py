30f07c179855ee4b9a16f3e63d98fb17fcab282b
katsu1110 <code1110g-show@hotmail.co.jp>
2018-02-17 16:35:35 +0100

    target distribution is ready

--- bayesbrain/mcmc.py
+++ bayesbrain/mcmc.py
@@ -9,33 +9,65 @@ Created on Sat Feb 10 17:20:26 2018
 # libraries
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.neighbors import KernelDensity
 
-# target distribution (multivariate Gaussian)
 def target_pdf(type):
+    """target distribution (multivariate Gaussian)"""
     if type == 'round':
         x, y = np.random.multivariate_normal([2,2], [[1,0],[0,1]], 5000).T
     elif type == 'correlated':
         x, y = np.random.multivariate_normal([2,2], [[1, 0.9], [0.9, 1]], 5000).T
-    elif type == 'bimodal':
-        x, y = 0.5*np.random.multivariate_normal([1,1], [[1,0],[0,1]], 5000).T \
-            + 0.5*np.random.multivariate_normal([3,3], [[1,0],[0,1]], 5000).T
     elif type == 'close_bimodal':
-        x, y = 0.5*np.random.multivariate_normal([1.25,1.25], [[1,0],[0,1]], 5000).T \
-            + 0.5*np.random.multivariate_normal([2.75,2.75], [[1,0],[0,1]], 5000).T
+        b = [0, 1]
+        x = np.array([])
+        y = np.array([])
+        for w in b:
+            xb, yb = w*np.random.multivariate_normal([0,0], [[1,0],[0,1]], 5000).T \
+                + (1-w)*np.random.multivariate_normal([7.5, 7.5], [[1,0],[0,1]], 5000).T
+            x = np.append(x, xb)
+            y = np.append(y, yb)
+    elif type == 'bimodal':
+        b = [0, 1]
+        x = np.array([])
+        y = np.array([])
+        for w in b:
+            xb, yb = w*np.random.multivariate_normal([0,0], [[1,0],[0,1]], 5000).T \
+                + (1-w)*np.random.multivariate_normal([10, 10], [[1,0],[0,1]], 5000).T
+            x = np.append(x, xb)
+            y = np.append(y, yb)
     return x, y
 
+def kde2D(x, y, bandwidth, xbins=100j, ybins=100j, **kwargs): 
+    """Build 2D kernel density estimate (KDE)."""
+
+    # create grid of sample locations (default: 100x100)
+    xx, yy = np.mgrid[x.min():x.max():xbins, 
+                      y.min():y.max():ybins]
+
+    xy_sample = np.vstack([yy.ravel(), xx.ravel()]).T
+    xy_train  = np.vstack([y, x]).T
+
+    kde_skl = KernelDensity(bandwidth=bandwidth, **kwargs)
+    kde_skl.fit(xy_train)
+
+    # score_samples() returns the log-likelihood of the samples
+    z = np.exp(kde_skl.score_samples(xy_sample))
+    return xx, yy, np.reshape(z, xx.shape)
+
 # visualization similar to Fig 1b in the Sanborn & Charter 2016
-type = ['round', 'correlated', 'bimodal', 'close_bimodal']
+type = ['round', 'correlated', 'close_bimodal', 'bimodal']
 plt.close('all')
 fig, axes = plt.subplots(1, 4)
-x, y = target_pdf(type[0])
-axes[0].plot(x,y,'o')
-x, y = target_pdf(type[1])
-axes[1].plot(x,y,'o')
-x, y = target_pdf(type[2])
-axes[2].plot(x,y,'o')
-x, y = target_pdf(type[3])
-axes[3].plot(x,y,'o')
-
+for i in range(4):
+    x, y = target_pdf(type[i])
+    xx, yy, zz = kde2D(x, y, 1)
+    axes[i].pcolormesh(xx, yy, zz)
+    axes[i].set_title(type[i])
+    axes[i].set_xlabel('x')
+    axes[i].set_ylabel('y')
+    axes[i].get_xaxis().set_ticks([])
+    axes[i].get_yaxis().set_ticks([])
+    axes[i].get_xaxis().set_ticklabels([])
+    axes[i].get_yaxis().set_ticklabels([])
 
 
