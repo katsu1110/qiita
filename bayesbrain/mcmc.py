5c11786b3d6b13ec3ca405a2d4a188a8431a4837
katsu1110 <code1110g-show@hotmail.co.jp>
2018-02-18 21:36:22 +0100

    simple implementation for metropolis hastings

--- bayesbrain/mcmc.py
+++ bayesbrain/mcmc.py
@@ -6,39 +6,49 @@ Created on Sat Feb 10 17:20:26 2018
 """
 
 # Markov Chain Monte Carlo (MCMC) --simple implementations
-# libraries
+# libraries ------------------------------
 import numpy as np
+from scipy.stats import multivariate_normal
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KernelDensity
 
-def target_pdf(type):
-    """target distribution (multivariate Gaussian)"""
-    if type == 'round':
-        x, y = np.random.multivariate_normal([2,2], [[1,0],[0,1]], 5000).T
-    elif type == 'correlated':
-        x, y = np.random.multivariate_normal([2,2], [[1, 0.9], [0.9, 1]], 5000).T
-    elif type == 'close_bimodal':
-        b = [0, 1]
+# functions -----------------------------
+def target_params(disttype):
+    """preset parameters for target distributions"""
+    if disttype == 'round':
+        me = [2,2]
+        cov = [[1,0],[0,1]]
+    elif disttype == 'correlated':
+        me = [2,2]
+        cov = [[1,0.9],[0.9,1]]
+    elif disttype == 'bimodal':
+        me = [[0,0],[6,6]]
+        cov = [[1,0],[0,1]]
+    elif disttype == 'close_bimodal':
+        me = [[0,0],[10,10]]
+        cov = [[1,0],[0,1]]
+    return me, cov
+    
+        
+def target_rvs(N, disttype):
+    """random samples from target distribution (bivariate Gaussian)"""
+    me, cov = target_params(disttype)
+    if disttype == 'round' or 'correlated':
+        p = multivariate_normal(mean=me, cov=cov).rvs(size=N, random_state=1220).T
+        x = p[0]
+        y = p[1]
+    elif disttype == 'bimodal' or 'close_bimodal':
         x = np.array([])
         y = np.array([])
-        for w in b:
-            xb, yb = w*np.random.multivariate_normal([0,0], [[1,0],[0,1]], 5000).T \
-                + (1-w)*np.random.multivariate_normal([7.5, 7.5], [[1,0],[0,1]], 5000).T
-            x = np.append(x, xb)
-            y = np.append(y, yb)
-    elif type == 'bimodal':
-        b = [0, 1]
-        x = np.array([])
-        y = np.array([])
-        for w in b:
-            xb, yb = w*np.random.multivariate_normal([0,0], [[1,0],[0,1]], 5000).T \
-                + (1-w)*np.random.multivariate_normal([10, 10], [[1,0],[0,1]], 5000).T
-            x = np.append(x, xb)
-            y = np.append(y, yb)
+        for w in range(1):
+            p = w*multivariate_normal(mean=me[0], cov=cov).rvs(size=round(N/2), random_state=1220).T \
+                + (1-w)*multivariate_normal(mean=me[1], cov=cov).rvs(size=int(N-round(N/2)), random_state=1220).T
+            x = np.append(x, p[0])
+            y = np.append(y, p[1])
     return x, y
 
 def kde2D(x, y, bandwidth, xbins=100j, ybins=100j, **kwargs): 
-    """Build 2D kernel density estimate (KDE)."""
+    """Build 2D kernel density estimate (KDE)"""
 
     # create grid of sample locations (default: 100x100)
     xx, yy = np.mgrid[x.min():x.max():xbins, 
@@ -54,15 +64,51 @@ def kde2D(x, y, bandwidth, xbins=100j, ybins=100j, **kwargs):
     z = np.exp(kde_skl.score_samples(xy_sample))
     return xx, yy, np.reshape(z, xx.shape)
 
-# visualization similar to Fig 1b in the Sanborn & Charter 2016
-type = ['round', 'correlated', 'close_bimodal', 'bimodal']
+def target_pdf(p, disttype):
+    """target distribution (bivariate Gaussian)"""
+    me, cov = target_params(disttype)
+    if disttype == 'round' or 'correlated':
+        prob = multivariate_normal.pdf(p, mean=me, cov=cov)
+    elif disttype == 'bimodal' or 'close_bimodal':
+        prob0 = multivariate_normal.pdf(p, mean=me[0], cov=cov)
+        prob1 = multivariate_normal.pdf(p, mean=me[1], cov=cov)
+        prob = max([prob0, prob1])        
+        
+    return prob
+
+def proposal_pdf(p):
+    """proposal distribution"""
+    return (p[0] + np.random.normal(0, 1), p[1] + np.random.normal(0, 1))
+
+def mh(N, disttype):
+    """metropolis hastings sampling (no 'thin' and 'burn_in' for simplicity)"""
+    xs = np.array([])
+    ys = np.array([])
+    pos_now = (0,0)
+    accept = 0
+    for i in range(N):
+        pos_cand = proposal_pdf(pos_now)
+        prob_stay = target_pdf(pos_now, disttype)
+        prob_move = target_pdf(pos_cand, disttype)
+        if prob_move / prob_stay > np.random.uniform(0,1,1):
+            pos_now = pos_cand
+            xs = np.append(xs, pos_now[0])
+            ys = np.append(ys, pos_now[1])
+            accept += 1
+    return xs, ys, accept/N
+
+# visualization similar to the Fig 1b in Sanborn & Charter 2016 -----------
+# preset parameters
+disttype = ['round', 'correlated', 'close_bimodal', 'bimodal']
 plt.close('all')
+
+# probability distributions
 fig, axes = plt.subplots(1, 4)
 for i in range(4):
-    x, y = target_pdf(type[i])
+    x, y = target_rvs(5000, disttype[i])
     xx, yy, zz = kde2D(x, y, 1)
     axes[i].pcolormesh(xx, yy, zz)
-    axes[i].set_title(type[i])
+    axes[i].set_title(disttype[i])
     axes[i].set_xlabel('x')
     axes[i].set_ylabel('y')
     axes[i].get_xaxis().set_ticks([])
