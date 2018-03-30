fb57a4e2bd2b6fff4c023900b79b282d98082605
katsu1110 <code1110g-show@hotmail.co.jp>
2018-03-30 20:41:01 +0200

    simulation finalized

--- evolution_bayesian/wright_fisher.py
+++ evolution_bayesian/wright_fisher.py
@@ -16,31 +16,32 @@ import matplotlib.animation as animation
 
 #%%
 # functions -----------------------------
-def wright_fisher_simulation(repeat, N, p):
+def wright_fisher_simulation(repeat, N, pAA, paa):
     '''simulation of Wright-Fisher model:
         repeat: the number of repeat in simulation
         N: the number of individuals in the population
-        p: the probability that gene A is held
+        pAA: the probability that an individual has AA
+        paa: the probability that an individual has aa
+        (hence the probability that an individual has Aa
+        is 1 - pAA - paa)
      '''   
-        # attribute allele 1, 1 to individuals as 1st generation
-    gene = {"allele1": np.random.choice([0, 1], N, replace=True, p=[1-p, p]),
-            "allele2": np.random.choice([0, 1], N, replace=True, p=[1-p, p])}
-    results = [gene]
+    # attribute allele to individuals as 1st generation
+    results = [[np.random.choice([0, 1, 2], N, replace=True, p=[paa, 1-pAA-paa, pAA])]]
     # iterate Wright-Fisher process
-    for r in np.arange(repeat-1):
-        p = (np.sum(gene["allele1"]==1) + np.sum(gene["allele2"]==1))/(2*N)
-        gene = {"allele1": np.random.choice([0, 1], N, replace=True, p=[1-p, p]),
-            "allele2": np.random.choice([0, 1], N, replace=True, p=[1-p, p])}
-        results.append(gene)
-    return results   
+    for r in np.arange(repeat-1)+1:
+        pAA = np.sum(results[r-1][0]==2)/N
+        pAa = np.sum(results[r-1][0]==1)/N
+        paa = np.sum(results[r-1][0]==0)/N
+        results.append([np.random.choice([0, 1, 2], N, replace=True, p=[paa, pAa, pAA])])
+    return results     
     
-def allele_color(a, b):
+def allele_color(x):
     '''color code for pairs of allele'''
-    if a==1 and b==1:
+    if x==2:
         col = "r"
-    elif (a==1 and b==0) or (a==0 and b==1):
+    elif x==1:
         col = "g"
-    elif a==0 and b==0:
+    elif x==0:
         col = "b"
     return col
 
@@ -48,10 +49,11 @@ def animate(i):
     '''for animation'''
     plt.cla()
     plt.axis('off')
-    plt.text(3.5, 6.7, "(P(A)=" + str(pa) + ")", fontsize=12, color="k", ha="left")
-    plt.text(0.5, 6.7, "AA", fontsize=20, color="r", ha="left")
-    plt.text(1.5, 6.7, "Ax", fontsize=20, color="g", ha="left")
-    plt.text(2.5, 6.7, "xx", fontsize=20, color="b", ha="left")
+    plt.text(2.3, 6.7, "P(AA), P(Aa), P(aa)=" + str(pAA) + ", " + str(round(100*(1-pAA-paa))/100) +
+                         ", " + str(paa), fontsize=12, color="k", ha="left")
+    plt.text(-0.5, 6.7, "AA", fontsize=20, color="r", ha="left")
+    plt.text(0.3, 6.7, "Aa", fontsize=20, color="g", ha="left")
+    plt.text(1.1, 6.7, "aa", fontsize=20, color="b", ha="left")
     plt.text(1.7, -0.9, "generation " + str(i), fontsize=20, color="k", ha="left")
     if i <= ngene:
         im  = plt.scatter(generation[i][0], generation[i][1], marker='o', s=300, 
@@ -65,8 +67,9 @@ def animate(i):
 # visualize simulation steps
 ngene = 100
 N = 49
-pa = 0.6
-results = wright_fisher_simulation(ngene, N, pa)
+pAA = 0.45
+paa = 0.25
+results = wright_fisher_simulation(ngene, N, pAA, paa)
 plt.close('all')
 fig = plt.figure()
 ims = []
@@ -76,13 +79,13 @@ if ncol**2 < N:
 else:
     nrow = ncol
 generation = []
-for gene in results:
+for g in range(ngene):
     plt.cla()
     x = [0]
     y = [0]
     c = []
     for n in np.arange(N):
-        c.append(allele_color(gene["allele1"][n], gene["allele2"][n]))
+        c.append(allele_color(results[g][0][n]))
         if x[-1]+1 >= ncol:
             x.append(0)
             y.append(y[-1]+1)
